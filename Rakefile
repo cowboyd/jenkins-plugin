b1856261d3cc02db0144e3ec842561318407c5c4
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-07-01 12:38:39 -0700

    hooking up a bootstrap for Rake task

--- Rakefile
+++ Rakefile
@@ -1,2 +1,5 @@
 require 'bundler'
 Bundler::GemHelper.install_tasks
+
+require 'jenkins/rake'
+Jenkins::Rake.install_tasks
--- lib/jenkins/rake.rb
+++ lib/jenkins/rake.rb
@@ -5,6 +5,7 @@ module Jenkins
       desc "package up stuff into HPI file"
       ::Rake::Task.define_task :package do
         puts "I'm not actually packaging anything"
+        puts RUBY_PLATFORM
       end
     end
   end
