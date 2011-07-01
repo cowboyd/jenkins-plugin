93b6424e00cd5cb638329b4a2aed7d8647ea0f41
Charles Lowell <cowboyd@thefrontside.net>
2011-07-01 09:42:15 -0500

    add a description to the task so that it shows up in the task help.

--- lib/jenkins/rake.rb
+++ lib/jenkins/rake.rb
@@ -2,6 +2,7 @@
 module Jenkins
   module Rake
     def self.install_tasks
+      desc "package up stuff into HPI file"
       ::Rake::Task.define_task :package do
         puts "I'm not actually packaging anything"
       end
