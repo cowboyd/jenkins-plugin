f4cf5f86e32228f252362ca52bc4cf924ca1d65b
Charles Lowell <cowboyd@thefrontside.net>
2011-07-31 00:44:29 -0500

    update requires to pull in almost everything.

--- lib/jenkins/plugins.rb
+++ lib/jenkins/plugins.rb
@@ -3,8 +3,12 @@ module Jenkins
     require 'jenkins/plugin'
     require 'jenkins/plugins/proxies'
     require 'jenkins/model'
+    require 'jenkins/model/action'
+    require 'jenkins/model/build'
     require 'jenkins/model/descriptor'
+    require 'jenkins/model/listener'
     require 'jenkins/slaves/cloud'
     require 'jenkins/tasks/build_wrapper'
+    require 'jenkins/launcher'
   end
 end
